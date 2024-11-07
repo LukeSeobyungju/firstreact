18e07bee8c07365ba9e39378957f0cc2ef22abc6
LukeSeobyungju <22100356@handong.ac.kr>
2024-11-07 22:38:14 +0900

    fix for deploy

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
       <header className="App-header">
         <h1 class="k1">大<br/>韓</h1>
         <h1 class="k2">民<br/>國</h1>
-        <img src="https://cdn-icons-png.flaticon.com/512/8147/8147536.png" className="Gun1" />
-        <img src="https://cdn-icons-png.flaticon.com/512/8147/8147536.png" className="Gun2" />
-        <img src="https://upload.wikimedia.org/wikipedia/commons/0/09/Flag_of_South_Korea.svg" className="App-logo" />
+        <img src="https://cdn-icons-png.flaticon.com/512/8147/8147536.png" className="Gun1" alt="gun"/>
+        <img src="https://cdn-icons-png.flaticon.com/512/8147/8147536.png" className="Gun2" alt="gun"/>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/0/09/Flag_of_South_Korea.svg" className="App-logo" alt="Korea"/>
         <br/>
         <br/>
         <br/>
